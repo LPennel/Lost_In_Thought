e4c2068fbad1ed2ad8c1e8c430abc69667371ad3
Spyro64bit <146040067+Spyro64bit@users.noreply.github.com>
2025-11-26 22:51:03 -0600

    No major changes

--- Assets/Scripts/ItemScripts/PencilScript.cs
+++ Assets/Scripts/ItemScripts/PencilScript.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class PencilScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    void OnTriggerStay2D(Collider2D other){
+        PlayerCombat controller = other.GetComponent<PlayerCombat>();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        controller.ChangeHealth(HealAmount);
+         Destroy(gameObject);
     }
 }
