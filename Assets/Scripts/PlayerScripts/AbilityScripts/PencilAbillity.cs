45b65e9b4450262b6c4dfd5c3dff3a626080675b
Lawson Pennel <lpennel@gmail.com>
2025-12-07 21:52:08 -0600

    Started Adding Art

--- Assets/Scripts/PlayerScripts/AbilityScripts/PencilAbillity.cs
+++ Assets/Scripts/PlayerScripts/AbilityScripts/PencilAbillity.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PencilAbillity : MonoBehaviour
 {
     public bool canUsePencil = false;
-
     public Vector2 pos;
+    private Renderer ren;
 
+    void Start()
+    {
+        ren = GetComponent<Renderer>();
+
+        if(ren != null)
+        {
+            ren.enabled = false;
+        }
+    }
     // Update is called once per frame
     void Update()
     {
         if(Input.GetButton("UsePencil") && canUsePencil == true){
             Cursor.visible = false;
+            
+            if(ren != null)
+            {
+                ren.enabled = true;
+            }
+
             Vector2 mouseCursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             transform.position = mouseCursorPos;
             pos = mouseCursorPos;
@@ -25,6 +41,11 @@ public class PencilAbillity : MonoBehaviour
             Cursor.visible = true;
             Time.timeScale = 1f;
             Time.fixedDeltaTime = 0.02f;
+
+            if(ren != null)
+            {
+                ren.enabled = false;
+            }
         }
     }
 }
